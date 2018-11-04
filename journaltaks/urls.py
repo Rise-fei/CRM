1280207f114684a39a95b00f95d37909520e0086
wangcf <18388627773@163.com>
2018-11-04 23:06:23 +0800

    w.1.1

--- journaltaks/urls.py
+++ journaltaks/urls.py
@@ -6,6 +6,14 @@ from journaltaks import views as j_vi
 
 urlpatterns = [
     url(r'journal/', j_vi.journal),
-    url(r'edit/(\d+)/', j_vi.edit),
+    url(r'editJournal/id=(\d+)/', j_vi.editJournal),
+    url(r'checkJournal/id=(\d+)/', j_vi.checkJournal),
+    url(r'newJournal/', j_vi.newJournal),
+    url(r'delJournal/id=(\d+)/', j_vi.delJournal),
+    url(r'bespeak/', j_vi.bespeak),
+    url(r'checkBespeak/id=(\d+)/', j_vi.checkBespeak),
+    url(r'editBespeak/id=(\d+)/', j_vi.editBespeak),
+    url(r'newBespeak/', j_vi.newBespeak),
+    url(r'afterSale/', j_vi.afterSale),
 
 ]
